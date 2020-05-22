a9d6c86c277bb14ff12dcf697313ed84cde29409
ViiSkor <vikaskorik@gmail.com>
2020-05-22 19:25:21 +0300

    Update metrics and losses

--- src/losses.py
+++ src/losses.py
@@ -1,21 +1,36 @@
-from metrics import dice_coefficient, tversky
+from metrics import dice_coefficient
 from tensorflow.keras.losses import binary_crossentropy
 
 
-def dice_loss(y_true, y_pred):
-    loss = 1 - dice_coefficient(y_true, y_pred)
-    return loss
+def soft_dice_loss(y_true, y_pred):
+  """Soft dice loss calculation for arbitrary batch size, number of classes, and number of spatial dimensions.
+  
+    Args:
+      y_true (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c One hot encoding of ground truth.
+      y_pred (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+    Returns:
+      (tf.tensor): Soft dice loss.
+  """
+
+  loss = 1 - dice_coefficient(y_true, y_pred)
+  return loss
 
 
 def bce_dice_loss(y_true, y_pred):
-    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
-    return loss
+  """Soft dice loss + binary crossentropy.
+  
+    Args:
+      y_true (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c One hot encoding of ground truth.
+      y_pred (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+    Returns:
+      (tf.tensor): loss.
+  """
+
+  loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+  return loss
 
 
-def tversky_loss(y_true, y_pred):
-    return 1 - tversky(y_true,y_pred)
-
-
-def focal_tversky(y_true, y_pred, gamma=0.75):
-    pt_1 = tversky(y_true, y_pred)
-    return K.pow((1-pt_1), gamma)
--- src/metrics.py
+++ src/metrics.py
@@ -1,29 +1,67 @@
+import numpy as np
 from tensorflow.keras import backend as K
 
-smooth = 1.
+epsilon=1e-6 # Used for numerical stability to avoid divide by zero errors
 
-def dice_coefficient(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return score
+
+def dice_coefficient(y_true, y_pred): 
+    """Dice coefficient calculation for arbitrary batch size, number of classes, and number of spatial dimensions.
+  
+    Args:
+      y_true (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c One hot encoding of ground truth.
+      y_pred (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+    Returns:
+      (tf.tensor): Dice coefficient.
+    References
+      Adapted from https://github.com/Lasagne/Recipes/issues/99#issuecomment-347775022
+    """
+    
+    # skip the batch and class axis for calculating Dice score
+    axes = tuple(range(1, len(y_pred.shape)-1))
+    numerator = 2. * K.sum(y_pred * y_true, axes)
+    denominator = K.sum(K.square(y_pred) + K.square(y_true), axes)
+    
+    return K.mean(numerator / (denominator + epsilon)) # average over classes and batch
 
 
 def confusion(y_true, y_pred):
-    y_pred_pos = K.clip(y_pred, 0, 1)
-    y_pred_neg = 1 - y_pred_pos
-    y_pos = K.clip(y_true, 0, 1)
-    y_neg = 1 - y_pos
-    tp = K.sum(y_pos * y_pred_pos)
-    fp = K.sum(y_neg * y_pred_pos)
-    fn = K.sum(y_pos * y_pred_neg) 
-    prec = (tp + smooth)/(tp+fp+smooth)
-    rec = (tp+smooth)/(tp+fn+smooth)
-    return prec, rec
+  """Confusion matrix.
+  
+  Args:
+  y_true (numpy.array/tf.tensor):
+    b x X x Y( x Z...) x c One hot encoding of ground truth.
+  y_pred (numpy.array/tf.tensor):
+    b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+  Returns:
+    (tf.tensor): precision, recall.
+  """
+
+  y_pred_pos = K.clip(y_pred, 0, 1)
+  y_pred_neg = 1 - y_pred_pos
+  y_pos = K.clip(y_true, 0, 1)
+  y_neg = 1 - y_pos
+  tp = K.sum(y_pos * y_pred_pos)
+  fp = K.sum(y_neg * y_pred_pos)
+  fn = K.sum(y_pos * y_pred_neg) 
+  prec = (tp)/(tp+fp+epsilon)
+  rec = (tp)/(tp+fn+epsilon)
+  return prec, rec
 
 
 def recall(y_true, y_pred):
+  """Recall=True_positive/(True_positive + False_negative).
+  
+  # Args:
+    y_true (numpy.array/tf.tensor):
+      b x X x Y( x Z...) x c One hot encoding of ground truth.
+    y_pred (numpy.array/tf.tensor):
+      b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+  Returns:
+    (tf.tensor): recall.
+  """
+
   y_pred_pos = K.clip(y_pred, 0, 1)
   y_pred_neg = 1 - y_pred_pos
   y_pos = K.clip(y_true, 0, 1)
@@ -31,11 +69,22 @@ def recall(y_true, y_pred):
   tp = K.sum(y_pos * y_pred_pos)
   fp = K.sum(y_neg * y_pred_pos)
   fn = K.sum(y_pos * y_pred_neg)
-  rec = (tp+smooth)/(tp+fn+smooth)
+  rec = (tp)/(tp+fn+epsilon)
   return rec
 
 
 def precision(y_true, y_pred):
+  """Precision=True_positive/(True_positive + False_positive).
+  
+  # Args:
+    y_true (numpy.array/tf.tensor):
+      b x X x Y( x Z...) x c One hot encoding of ground truth.
+    y_pred (numpy.array/tf.tensor):
+      b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+  Returns:
+    (tf.tensor): precision.
+  """
+
   y_pred_pos = K.clip(y_pred, 0, 1)
   y_pred_neg = 1 - y_pred_pos
   y_pos = K.clip(y_true, 0, 1)
@@ -43,14 +92,44 @@ def precision(y_true, y_pred):
   tp = K.sum(y_pos * y_pred_pos)
   fp = K.sum(y_neg * y_pred_pos)
   fn = K.sum(y_pos * y_pred_neg)
-  prec = (tp + smooth)/(tp+fp+smooth)
+  prec = (tp)/(tp+fp+epsilon)
   return prec
 
 
-def tversky(y_true, y_pred, alpha=0.7):
-    y_true_pos = K.flatten(y_true)
-    y_pred_pos = K.flatten(y_pred)
-    true_pos = K.sum(y_true_pos * y_pred_pos)
-    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
-    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
-    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+def iou(y_true, y_pred): 
+    """Intersection over Union for arbitrary batch size, number of classes, and number of spatial dimensions.
+  
+    Args:
+      y_true (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c One hot encoding of ground truth.
+      y_pred (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+    Returns:
+      (tf.tensor): IoU coefficient.
+    """
+
+    # skip the batch and class axis for calculating IoU score
+    axes = tuple(range(1, len(y_pred.shape)-1))
+    numerator = K.sum(y_pred * y_true, axes)
+    denominator = K.sum(K.square(y_pred) + K.square(y_true), axes)
+    return K.mean(numerator / (denominator + epsilon)) # average over classes and batch
+
+
+def mean_iou(y_true, y_pred):
+    """Mean of Intersection over Union.
+    Thresholds for mask are from 0.5, to 1.0 with a step 0.5.
+  
+    Args:
+      y_true (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c One hot encoding of ground truth.
+      y_pred (numpy.array/tf.tensor):
+        b x X x Y( x Z...) x c Network output, must sum to 1 over c channel.
+    Returns:
+      (tf.tensor): Mean IoU coefficient.
+    """
+
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+      y_pred = K.cast(K.greater(y_pred, t), dtype='float32')
+      score = iou(y_true, y_pred)
+      prec.append(score)
